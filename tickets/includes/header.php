ab4795aa63be09a193113bf135b2d186276094f1
Joel Roberto Chan Mendez <120886127+JoelChanMendez@users.noreply.github.com>
2023-09-30 20:56:17 -0600

    Add files via upload

---
+++ tickets/includes/header.php
@@ -0,0 +1,8 @@
+<nav class="navbar bg-dark ">
+    <div class="container-fluid">
+    <a class="navbar-brand text-light " href="#">
+        <img src="https://img.freepik.com/vector-gratis/vector-diseno-degradado-colorido-pajaro_343694-2506.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top  ">
+        Sistema de tickets
+    </a>
+    </div>
+</nav>
